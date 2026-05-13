bd980314bcfcb1bc09a98f593cca127464a6d861
Devell Robinson <devell.robinson@bordevtech.com>
2026-05-13 14:11:29 -0400

    refactor: simplify color registry by removing unused color variants

--- packages/palette/src/color-registry.ts
+++ packages/palette/src/color-registry.ts
@@ -3,15 +3,7 @@ import * as base from "./index";
 export const colorRegistry = {
     primary: {
         red: `rgb(${base.palette.red[500]})`,
-        light: `rgb(${base.palette.red[300]})`,
-        dark: `rgb(${base.palette.red[700]})`,
-
-        blue: base.palette.green[500],
-        light: base.palette.green[300],
-        dark: base.palette.green[700],
-
-        green: base.palette.blue[500],
-        light: base.palette.blue[300],
-        dark: base.palette.blue[700]
+        green: base.palette.green[500],
+        blue: base.palette.blue[500],
     },
 };
